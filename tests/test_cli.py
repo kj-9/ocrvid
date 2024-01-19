8328fe06e952fd24a6bcb97645ffe3fbb31f0b53
Kj <46800811+kj-9@users.noreply.github.com>
2024-01-19 22:03:00 +0900

    add tests

---
+++ tests/test_cli.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from click.testing import CliRunner
+
+from ocrvid.cli import cli
+
+
+# testing the cli, key command group
+def test_lang_command():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["langs"])
+
+    assert result.exit_code == 0
+    assert result.output != ""
+
+def test_run_command():
+     runner = CliRunner()
+     with runner.isolated_filesystem():
+        input_file = str(Path(__file__).parent / "video/pexels-eva-elijas.mp4")
+        result = runner.invoke(cli, ["run", input_file])
+        
+        assert result.exit_code == 0
+        assert (Path.cwd() / "pexels-eva-elijas.json").exists()
+        assert (Path.cwd() / ".ocrvid/frames/pexels-eva-elijas").is_dir()
+        
+        result = runner.invoke(cli, ["detect", ".ocrvid/frames/pexels-eva-elijas/frame-0.png"])
+        assert result.exit_code == 0
+        assert result.output != ""
+
+
