e9f8d9d15aa185aff12207b6d4cbf551e90327b8
Kj <46800811+kj-9@users.noreply.github.com>
2024-02-12 13:26:44 +0900

    add test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,7 @@
+import json
 from pathlib import Path
 
+import pytest
 from click.testing import CliRunner
 
 from ocrvid.cli import cli
@@ -14,10 +16,23 @@ def test_lang_command():
     assert result.output != ""
 
 
-def test_run_command_with_frames_dir():
+@pytest.fixture
+def input_file():
+    return str(Path(__file__).parent / "video/pexels-eva-elijas.mp4")
+
+
+def test_props_command(input_file):
     runner = CliRunner()
     with runner.isolated_filesystem():
-        input_file = str(Path(__file__).parent / "video/pexels-eva-elijas.mp4")
+        result = runner.invoke(cli, ["props", input_file])
+
+        assert result.exit_code == 0
+        assert result.output != ""
+
+
+def test_run_command_with_frames_dir(input_file):
+    runner = CliRunner()
+    with runner.isolated_filesystem():
         frames_dir_str = ".ocrvid/frames/pexels-eva-elijas"
         result = runner.invoke(cli, ["run", input_file, "-fd", frames_dir_str])
 
@@ -30,10 +45,9 @@ def test_run_command_with_frames_dir():
         assert result.output != ""
 
 
-def test_run_command_with_custom_path():
+def test_run_command_with_custom_path(input_file):
     runner = CliRunner()
     with runner.isolated_filesystem():
-        input_file = str(Path(__file__).parent / "video/pexels-eva-elijas.mp4")
         result = runner.invoke(
             cli,
             ["run", input_file, "-o", "some/custom.json", "-fd", "custom-frame-dir"],
@@ -42,3 +56,16 @@ def test_run_command_with_custom_path():
         assert result.exit_code == 0
         assert (Path.cwd() / "some/custom.json").exists()
         assert (Path.cwd() / "custom-frame-dir").is_dir()
+
+
+def test_run_command_with_by_second(input_file):
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ["run", input_file, "-bs", "1"])
+
+        assert result.exit_code == 0
+        assert (Path.cwd() / "pexels-eva-elijas.json").exists()
+
+        with open(Path.cwd() / "pexels-eva-elijas.json") as f:
+            data = json.load(f)
+            assert len(data["frames"]) == 24  # 24 seconds in the video
