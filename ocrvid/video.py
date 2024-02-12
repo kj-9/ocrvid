6feafda294bd9d60cc07da55738907da9a1492d3
Kj <46800811+kj-9@users.noreply.github.com>
2024-02-12 13:23:34 +0900

    add docs and fix indent

--- ocrvid/video.py
+++ ocrvid/video.py
@@ -39,6 +39,15 @@ class Video:
     frame_prefix: t.ClassVar[str] = "frame-"  # prefix for frame files
 
     def frame_generator(self, frame_step: int):
+        """
+        Generates frames from a video file.
+
+        Args:
+            frame_step (int): The step size between frames to be generated.
+
+        Yields:
+            Tuple[int, numpy.ndarray]: A tuple containing the index of the frame and the frame itself.
+        """
         with VideoCapture(str(self.video_file)) as vid:
             index = 0
 
@@ -56,6 +65,18 @@ class Video:
                 index += 1
 
     def get_props(self) -> dict[str, t.Optional[float]]:
+        """
+        Retrieve the properties of the video file.
+
+        Returns:
+            A dictionary containing the following properties:
+            - frame_count: The total number of frames in the video.
+            - fps: The frames per second of the video.
+            - seconds: The duration of the video in seconds.
+            - width: The width of each frame in pixels.
+            - height: The height of each frame in pixels.
+        """
+
         with VideoCapture(str(self.video_file)) as vid:
 
             def get_or_none(prop: int):
@@ -87,11 +108,18 @@ class Video:
         frames_dir: t.Optional[Path] = None,
         langs: t.Optional[t.List[str]] = None,
     ) -> t.List[Frame]:
-        """Run OCR on frames and return a list of Frame objects
+        """
+        Run OCR on the video frames.
+        Must provide either frame_step or by_second.
 
         Args:
-            frames_dir (Path, optional): where to save frames. Defaults to None i.e. not save frames.
-            langs (t.Optional[t.List[str]], optional): prefered languages to detect, ordered by priority. Defaults to None i.e. auto detect.
+            frame_step (int, optional): The number of frames to skip between OCR operations. Defaults to None.
+            by_second (int, optional): The number of seconds between OCR operations. Defaults to None.
+            frames_dir (Path, optional): The directory to save the extracted frames. Defaults to None.
+            langs (List[str], optional): The list of languages to use for OCR. Defaults to None which means auto detect.
+
+        Returns:
+            List[Frame]: The list of Frame objects containing OCR results.
         """
 
         if by_second:
